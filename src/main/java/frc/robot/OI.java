171b98d0862bd5f6ef391ebe85221ce2ad33d3e9
Steven Wang <steven.wang355@gmail.com>
2019-01-18 13:46:59 -0500

    +4motors, +arcadeDrive, +advanced deadZone, +slowMode
    4 motors to use for driving (ports yet to be set), added arcade drive, separate from tank drive, with constants to change speed. Added advanced dead zone which scales the current based on being past the deadzone with linear functions. Added slowMode boolean and command to perform precision driving.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.commands.SlowMode;
 
 /**
  * Add your docs here.
@@ -15,7 +17,14 @@ import edu.wpi.first.wpilibj.XboxController;
 public class OI {
 
     public static final int CONTROLLER_PORT = 0;
-    public static final XboxController controller1 = new XboxController(CONTROLLER_PORT);
+    public static final int X_ID = 3;
+
+    private static final XboxController controller1 = new XboxController(CONTROLLER_PORT);
+    private static final JoystickButton X_BUTTON = new JoystickButton(controller1, X_ID);
+
+    public OI() {
+        X_BUTTON.whenPressed(new SlowMode());
+    }
 
     public XboxController getController() {
         return controller1;
--- src/main/java/frc/subsystems/DriveTrain.java
+++ src/main/java/frc/subsystems/DriveTrain.java
@@ -9,7 +9,8 @@ package frc.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.commands.DriveWithController;
@@ -21,14 +22,31 @@ import frc.robot.Robot;
 public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-    public static final int TALON_ONE_PORT = 0;
-    public static final int TALON_TWO_PORT = 1;
+  public static final int LEFT_FRONT = 0;
+  public static final int RIGHT_FRONT = 1;
+  public static final int LEFT_BACK = 2;
+  public static final int RIGHT_BACK = 3;
 
-    private static final WPI_TalonSRX talon1 = new WPI_TalonSRX(TALON_ONE_PORT);
-    private static final WPI_TalonSRX talon2 = new WPI_TalonSRX(TALON_TWO_PORT);
-    public static DifferentialDrive drive = new DifferentialDrive(talon1, talon2);
-    
-    public static final double deadZoneConstant = 0.2;
+  private static final WPI_TalonSRX leftFront = new WPI_TalonSRX(LEFT_FRONT);
+  private static final WPI_TalonSRX rightFront = new WPI_TalonSRX(RIGHT_FRONT);
+  private static final WPI_TalonSRX leftBack = new WPI_TalonSRX(LEFT_BACK);
+  private static final WPI_TalonSRX rightBack = new WPI_TalonSRX(RIGHT_BACK);
+
+  public static final SpeedControllerGroup left = new SpeedControllerGroup(leftFront, leftBack);
+  public static final SpeedControllerGroup right = new SpeedControllerGroup(rightFront, rightBack);
+
+  public static final double DEAD_ZONE = 0.2;
+
+  // slows down the robot so that the robot is not too fast
+  public static final double JOYSTICK_CONSTANT = 0.7;
+  public static final double TRIGGERS_CONSTANT = 0.7;
+
+  // these slow down the robot for precision driving
+  static boolean slowMode = false;
+  public static final double SLOW_MODE_JOYSTICK = 0.7;
+  public static final double SLOW_MODE_TRIGGERS = 0.8;
+
+  private static final DifferentialDrive drive = new DifferentialDrive(left, right);
 
   @Override
   public void initDefaultCommand() {
@@ -37,37 +55,47 @@ public class DriveTrain extends Subsystem {
 
     setDefaultCommand(new DriveWithController());
   }
-  public static boolean deadZoneLeft(double leftY) {
-    if (Math.abs(leftY) < deadZoneConstant) {
-      return true;
-    }
-    else{
-      return false;
-    }
 
-  }
-  public static boolean deadZoneRight(double rightY) {
-    if (Math.abs(rightY) < deadZoneConstant) {
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-  // implement this to drive with a controller
-  public void drive() {
-    double leftY = -Robot.m_oi.getController().getY(GenericHID.Hand.kLeft);
-    double rightY = -Robot.m_oi.getController().getY(GenericHID.Hand.kRight);
+  /** Checks if the triggers are not being used at all, returns the current to use for driving */
+  public static double deadZone(double current) {
+    if (Math.abs(current) < DEAD_ZONE) return 0;
     
-    if (deadZoneLeft(leftY)) {
-      leftY = 0;
-    }
-    if (deadZoneLeft(rightY)) {
-      rightY = 0;
-    }
+    return (current - DEAD_ZONE * (current > 0d ? 1d : -1d)) / (1d - DEAD_ZONE);
+  }
+
+  // implement this to drive with a controller
+  public void tankDrive() {
+    double leftY = -Robot.m_oi.getController().getY(Hand.kLeft);
+    double rightY = -Robot.m_oi.getController().getY(Hand.kRight);
+    
+    leftY = deadZone(leftY);
+    rightY = deadZone(leftY);
+
+    leftY = leftY * (slowMode ? SLOW_MODE_JOYSTICK : 1d) * JOYSTICK_CONSTANT;
+    rightY = rightY * (slowMode ? SLOW_MODE_JOYSTICK : 1d) * JOYSTICK_CONSTANT;
+
     drive.tankDrive(leftY, rightY);
   }
+
+  public void arcadeDrive() {
+    double leftY = -Robot.m_oi.getController().getY(Hand.kLeft);
+    double leftTrigger = Robot.m_oi.getController().getTriggerAxis(Hand.kLeft);
+    double rightTrigger = Robot.m_oi.getController().getTriggerAxis(Hand.kRight);
+
+    leftY = deadZone(leftY);
+
+    leftY = leftY * (slowMode ? SLOW_MODE_JOYSTICK : 1d) * JOYSTICK_CONSTANT;
+    double rotation = (rightTrigger - leftTrigger) *
+        (slowMode ? SLOW_MODE_TRIGGERS : 1d) * TRIGGERS_CONSTANT;
+
+    drive.arcadeDrive(leftY, rotation);
+  }
+
   public void stop() {
     drive.tankDrive(0, 0);
+  }
+
+  public void switchSlowMode() {
+    slowMode = !slowMode;
   }
 }
