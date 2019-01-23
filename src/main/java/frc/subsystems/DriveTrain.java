582d7eed7009dddd620d2f460799bc3c5c63f67f
Chris Beeler <chrisbeeler627@comcast.net>
2019-01-23 14:20:23 -0500

    changees

--- src/main/java/frc/subsystems/DriveTrain.java
+++ src/main/java/frc/subsystems/DriveTrain.java
@@ -23,20 +23,20 @@ import frc.robot.Robot;
 public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-    public static final int TALON_ONE_PORT = 0;
-    public static final int TALON_TWO_PORT = 1;
-    public static final int TALON_THREE_PORT = 2;
-    public static final int TALON_FOUR_PORT = 3;
+    public static final int LEFT_BACK = 1;
+    public static final int LEFT_FRONT = 4;
+    public static final int RIGHT_BACK = 5;
+    public static final int RIGHT_FRONT = 2;
     
 
-    private static final WPI_TalonSRX talon1 = new WPI_TalonSRX(TALON_ONE_PORT);
-    private static final WPI_TalonSRX talon2 = new WPI_TalonSRX(TALON_TWO_PORT);
-    private static final WPI_TalonSRX talon3 = new WPI_TalonSRX(TALON_THREE_PORT);
-    private static final WPI_TalonSRX talon4 = new WPI_TalonSRX(TALON_FOUR_PORT);
+    private static final WPI_TalonSRX leftBack = new WPI_TalonSRX(LEFT_BACK);
+    private static final WPI_TalonSRX leftFront = new WPI_TalonSRX(LEFT_FRONT);
+    private static final WPI_TalonSRX rightBack = new WPI_TalonSRX(RIGHT_BACK);
+    private static final WPI_TalonSRX rightFront = new WPI_TalonSRX(RIGHT_FRONT);
     public static double LeftY = 0;
-    public static final SpeedControllerGroup Group1 = new SpeedControllerGroup(talon1, talon3);
-    public static final SpeedControllerGroup Group2 = new SpeedControllerGroup(talon2, talon4);
-    public static final DifferentialDrive drive = new DifferentialDrive(Group1, Group2);
+    public static final SpeedControllerGroup left = new SpeedControllerGroup(leftBack, leftFront);
+    public static final SpeedControllerGroup right = new SpeedControllerGroup(rightBack, rightFront);
+    public static final DifferentialDrive drive = new DifferentialDrive(left, right);
     public static double RightY = 0;
     boolean Slow = false;
     int clock = 0;
@@ -48,9 +48,9 @@ public class DriveTrain extends Subsystem {
 
     setDefaultCommand(new DriveWithController());
   }
-  public void setslowmode(boolean mode) {
+  // public void setslowmode(boolean mode) {
 
-  }
+  // }
   // implement this to drive with a controller
   public void tankDrive() {
     
@@ -72,31 +72,30 @@ public class DriveTrain extends Subsystem {
    
    //Cheking for slowmode
    
-   if (clock<50) {
+  //  if (clock<50) {
 
-    clock += 1;
+  //   clock += 1;
 
-   }
-    else {
-      clock = 0;
-      boolean CheckSlow = Robot.m_oi.getController().getYButtonPressed();
-    if (CheckSlow) {
-      Slow=!Slow;
-    }
-  }
-    //Exacuting slow mode
+  //  }
+  //   else {
+  //     clock = 0;
+  //     boolean CheckSlow = Robot.m_oi.getController().getYButtonPressed();
+  //   if (CheckSlow) {
+  //     Slow=!Slow;
+  //   }
+  // }
+  //   //Exacuting slow mode
   
-    while (Slow) {
+  //   while (Slow) {
 
-      LeftY = LeftY / 2;
-      RightY = RightY / 2;
+  //     LeftY = LeftY / 2;
+  //     RightY = RightY / 2;
 
-    }
+  //   }
     
     //Output
    
     drive.tankDrive(LeftY, RightY);
-
 
   }
   public void arcadedrive() {
