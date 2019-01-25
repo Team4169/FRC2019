b9b6902f4952f411751fb869c9b01f6156dd9619
Steven Wang <steven.wang355@gmail.com>
2019-01-25 17:52:40 -0500

    Copied code from CTRE for driving straight
    Changed joystick to XboxController
    Added 2 talons (front) and set them to follow the master talons (back)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,14 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 /**
  * Simple class containing constants used throughout project
  */
-package frc.subsystems;
+package frc.robot;
 
 public class Constants {
 	/**
